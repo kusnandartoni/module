721e4cd3c22fe6d955ecec0920f29931337aa6ca
kusnandartoni <kusnandartoni@gmail.com>
2018-07-18 07:02:54 +0700

    card list

--- src/pages/beginner/beginner.ts
+++ src/pages/beginner/beginner.ts
@@ -14,12 +14,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'beginner.html',
 })
 export class BeginnerPage {
-
+  public x: any =[
+    1,2,3,4,5,6,7,8,9
+  ]
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad BeginnerPage');
+    // console.log('ionViewDidLoad BeginnerPage');
+  }
+
+  detail(){
+    window.alert('aaa');
+    console.log('aaaaa')
   }
 
 }
